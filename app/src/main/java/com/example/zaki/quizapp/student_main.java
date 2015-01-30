e7b035c8dcd7e2915b7f66b97552888c2f35fab8
Tarek_Elgafy <tarekkzero@gmail.com>
2015-01-30 19:27:06 +0200

    try

---
+++ app/src/main/java/com/example/zaki/quizapp/student_main.java
@@ -0,0 +1,21 @@
+package com.example.zaki.quizapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zaki on 1/30/2015.
+ */
+public class student_main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.student_main);
+
+
+
+
+    }
+}
