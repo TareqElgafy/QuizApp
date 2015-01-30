cb4b46e99e0847cfd349e38bd521094b056b9907
Tarek_Elgafy <tarekkzero@gmail.com>
2015-01-30 17:07:14 +0200

    try

---
+++ app/src/main/java/com/example/zaki/quizapp/Instructor_log_in.java
@@ -0,0 +1,19 @@
+package com.example.zaki.quizapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zaki on 1/30/2015.
+ */
+public class Instructor_log_in extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructor_log_in);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/zaki/quizapp/student_log_in.java
@@ -0,0 +1,19 @@
+package com.example.zaki.quizapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zaki on 1/30/2015.
+ */
+public class student_log_in extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_log_in);
+
+
+    }
+
+}
